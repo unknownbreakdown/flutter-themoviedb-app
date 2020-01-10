7cfbef9d373205a46225a75f511d1b8c1d1bb172
CBE <unknownbreakdown@gmail.com>
2020-01-09 23:33:58 -0500

    Build a card swiper

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text("Hello"),
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text("The Movie DB App"),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _buildCardSwiper(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCardSwiper() {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        layout: SwiperLayout.STACK,
+        itemWidth: 240.0,
+        itemBuilder: (BuildContext context, int index) {
+          return new Image.network(
+            "http://via.placeholder.com/350x150",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        //pagination: new SwiperPagination(),
+        //control: new SwiperControl(),
       ),
     );
   }
